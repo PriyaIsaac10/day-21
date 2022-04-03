62cbc5a71b0a65fbda44513028688f3b5ca27de2
PriyaIsaac10 <99639818+PriyaIsaac10@users.noreply.github.com>
2022-04-03 16:53:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+var i = 10;
+setTimeout(function(){
+document.getElementById('text').innerHTML = "10";
+
+setTimeout(function(){
+ document.getElementById('text').innerHTML = "9";
+
+ setTimeout(function(){
+ document.getElementById('text').innerHTML = "8";
+
+setTimeout(function(){
+document.getElementById('text').innerHTML = "7";
+
+setTimeout(function(){
+document.getElementById('text').innerHTML = "6";
+
+setTimeout(function(){
+document.getElementById('text').innerHTML = "5";
+
+setTimeout(function(){
+document.getElementById('text').innerHTML = "4";
+
+setTimeout(function(){
+document.getElementById('text').innerHTML = "3";
+
+setTimeout(function(){
+document.getElementById('text').innerHTML = "2";
+
+setTimeout(function(){
+document.getElementById('text').innerHTML = "1";
+
+setTimeout(function(){
+document.getElementById('text').innerHTML = "0";
+
+setTimeout(function(){
+document.getElementById('text').innerHTML = "Happy Independence Day...!!!";
+setTimeout(function(){
+},1200)
+},1200)
+},1200)
+},1200)
+},1200)
+},1200)
+},1200)
+},1200)                         
+},1200)
+},1200)
+},1200)
+},1200)
+},1200)
